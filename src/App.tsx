63c572ff6358e6f56865d6ace4287b9e3c85d9fb
Maksym <smolinskimaksym@gmail.com>
2024-11-01 16:32:03 +0100

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Header } from "@/core/components/header";
+
+import { ParkingMap } from "@/features/parking-map/components/parking-map/parking-map.tsx";
 
 function App() {
-  return <div>Initial project setup</div>;
+  return (
+    <div className="container mx-auto min-h-screen py-12">
+      <Header />
+      <main className="flex flex-col items-center mt-16 space-y-4 w-full">
+        <ParkingMap />
+      </main>
+    </div>
+  );
 }
 
 export default App;
