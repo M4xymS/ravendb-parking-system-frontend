870516db2588b6ab882609abb48aaa2216985fc9
Maksym <smolinskimaksym@gmail.com>
2024-11-01 16:39:02 +0100

    feat: Added background color and slotName change on hover for parking slots to improve UI feedback.

--- src/features/parking-map/components/parking-slot/parking-slot.tsx
+++ src/features/parking-map/components/parking-slot/parking-slot.tsx
@@ -11,10 +11,13 @@ interface ParkingSlotProps {
 export const ParkingSlot: FC<ParkingSlotProps> = ({ slot, isOccupied, index }) => {
   return (
     <button
-      className={cn("relative w-24 h-12 border flex items-center justify-center", {
-        "border-r-0": index % 2 === 1,
-        "border-l-0": index % 2 === 0,
-      })}
+      className={cn(
+        "relative group hover:bg-muted w-24 transition-colors h-12 border flex items-center justify-center",
+        {
+          "border-r-0": index % 2 === 1,
+          "border-l-0": index % 2 === 0,
+        }
+      )}
     >
       {isOccupied ? (
         <img
@@ -23,7 +26,7 @@ export const ParkingSlot: FC<ParkingSlotProps> = ({ slot, isOccupied, index }) =
           className={cn("", { "rotate-180": index % 2 === 1 })}
         />
       ) : (
-        <span className="text-xs font-medium text-gray-400">{slot}</span>
+        <span className="text-xs group-hover:text-black font-medium text-gray-400">{slot}</span>
       )}
     </button>
   );
