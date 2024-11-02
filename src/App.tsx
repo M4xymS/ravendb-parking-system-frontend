b05eea93c4e6b8433eb7d0ac3585ab1b415f5597
Maksym <smolinskimaksym@gmail.com>
2024-11-02 21:27:54 +0100

    added ui components, hooks and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/core/components/header";
+import { Toaster } from "@/core/components/ui/toaster.tsx";
 
 import { ParkingMap } from "@/features/parking-map/components/parking-map/parking-map.tsx";
 import { ParkingSidebar } from "@/features/parking-sidebar/components/parking-sidebar/parking-sidebar.tsx";
@@ -15,6 +16,7 @@ function App() {
           </main>
         </div>
       </div>
+      <Toaster />
     </div>
   );
 }
---
+++ src/core/hooks/index.ts
@@ -0,0 +1,2 @@
+export { useToast } from "./use-toast";
+export { useMediaQuery } from "./use-media-query";
